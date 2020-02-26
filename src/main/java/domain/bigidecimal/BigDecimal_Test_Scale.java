aa62153ae5766558b6f6e930cd18ba9f15834ce0
rami2076 <sakusennx@yahoo.co.jp>
2020-02-26 18:03:31 +0900

    first commit

---
+++ src/main/java/domain/bigidecimal/BigDecimal_Test_Scale.java
@@ -0,0 +1,11 @@
+package domain.bigidecimal;
+
+import java.math.BigDecimal;
+
+public class BigDecimal_Test_Scale {
+
+    public static void main(String[] args) {
+      
+        System.out.println(BigDecimal.ZERO.setScale(2).toString());
+    }
+}
---
+++ src/main/java/domain/stringutils/StringUtilsTest.java
@@ -0,0 +1,10 @@
+package domain.stringutils;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtilsTest {
+    public static void main(String...strings ) {
+        
+        StringUtils.isBlank("");
+    }
+}
